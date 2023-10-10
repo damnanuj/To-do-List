56739945d7fea22b01bb8b6ab3e0f021ad26b089
Anuj <anujkrgupta21@gmail.com>
2023-10-10 18:49:11 +0530

    added transistion

--- script.js
+++ script.js
@@ -9,14 +9,19 @@
 const addbtn = document.querySelector(".button");
 const inputbox = document.querySelector(".input-row");
 const hider =document.getElementById("blurBackground");
+const lists = document.querySelector(".ListArea");
 const blurEffect =  document.getElementById("blurBackground");
 
+// addbtn.addEventListener("click", () => {
+//     inputbox.classList.add("inputVisible")
+//     blurEffect.style.display = "block";
+// });
 addbtn.addEventListener("click", () => {
-    inputbox.classList.add("inputVisible")
-    blurEffect.style.display = "block";
+    // lists.style.display = "block";
+    lists.style.width= "293px";;
 });
 
-hider.addEventListener("click", () => {
-    inputbox.classList.remove("inputVisible")
-    blurEffect.style.display = "none";
-})
+// hider.addEventListener("click", () => {
+//     inputbox.classList.remove("inputVisible")
+//     blurEffect.style.display = "none";
+// })
